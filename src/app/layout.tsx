5c168172fe1e8da8462e8a10f3c1f67d1f42411b
Malik Teague <malikisiah214@gmail.com>
2024-12-12 17:02:31 -0600

    updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,10 @@ import 'katex/dist/katex.min.css';
 export const metadata: Metadata = {
   title: {
     template: '%s - Malik Teague',
-    default: 'Malik Teague - Software designer, founder, and amateur astronaut',
+    default: 'Malik Teague - Software designer, entrepreneur, and athlete',
   },
   description:
-    'I’m Malik, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Malik, a software designer and entrepreneur based in Chicago.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { redirect } from 'next/navigation'
+import { redirect } from 'next/navigation';
+import { type Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'About',
+  description:
+    'I’m Malik Teague. I live in Chicago, where I turn ideas into reality.',
+};
 
 export default function Page() {
-  redirect('/about')
+  redirect('/about');
 }
