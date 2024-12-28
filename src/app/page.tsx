6af7a7e751ca236a51976658a2e741d91d6c8ba8
Malik Teague <malikisiah214@gmail.com>
2024-12-27 21:22:07 -0600

    updated metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 import { type Metadata } from 'next';
 export const metadata: Metadata = {
-  title: 'About',
+  title: 'Malik Teague',
   description:
     'I’m Malik Teague. I live in Chicago, where I turn ideas into reality.',
 };
